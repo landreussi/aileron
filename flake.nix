1d53feedb9fbc2a7e66bf79ac0619f6477f2e4c4
Lucas Andreussi <lucasandreussi@gmail.com>
2025-12-29 09:58:27 -0300

    feat: nix stuff and rust confs

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Rust development environment with stable toolchain via rustup";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          clang
+          llvmPackages.libclang
+          rustup
+          cargo
+          pkg-config
+          openssl
+        ];
+      };
+    });
+}
